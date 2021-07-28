4b6e428ea5c32bac65af9ea9ff9eae2051284e87
aman <amanproff@gmail.com>
2021-07-29 03:11:13 +0530

    changes made to project tab css

--- src/Projects/Projects.jsx
+++ src/Projects/Projects.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import "./project.css"
 
 const Projects = () => {
   return (
-    <div>
-      This is my project Page
+    <div className="temporary_div">
+      <h2>
+        This is my project Page
+      </h2>
     </div>
   )
 }
